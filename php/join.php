9dc3c4a56903780005419fd2a0b7e64e3166c302
CHO HYE RA <s2037@e-mirim.hs.kr>
2021-11-22 10:14:03 +0900

    :dolphin: 관리자 정보 저장

--- php/join.php
+++ php/join.php
@@ -8,13 +8,13 @@ $db = "earth";
 $conn = mysqli_connect($url,$id,$password,$db);
 
 // html로부터 값 가져오기
-$uemail = $_POST["email"];
+$uid = $_POST["id"];
 $upw = $_POST["pw"];
 $upwchk = $_POST["pwchk"];
 $uname = $_POST["name"];
 $ugender = $_POST["gender"];
 
-$sql = "insert into user(name, email, pw, gender) values('$uname', '$uemail', '$upw', '$ugender')";
+$sql = "insert into user(name, uid, pw, gender) values('$uname', '$uid', '$upw', '$ugender')";
 mysqli_query($conn, $sql);
 
 ?>
