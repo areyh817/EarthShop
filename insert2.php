fd1305cf37ec77f9e069c373fd155f2b610bd83a
CHO HYE RA <s2037@e-mirim.hs.kr>
2021-12-02 22:34:19 +0900

    상품 이미지가 아주 잘 들어감 ~~ :heart:

--- insert2.php
+++ insert2.php
@@ -13,23 +13,26 @@ $pdesc = $_POST["pdesc"];
 $country = $_POST["pcountry"];
 $price = $_POST["pprice"];
 $brand = $_POST["pbrand"];
-echo $name." ".$desc." ".$country." ".$price." ".$brand;
 
 
 $uploads_dir='uploads/';
 $upload_file=$uploads_dir.basename($_FILES['img']['name']);
+$upload_file2=$uploads_dir.basename($_FILES['img2']['name']);
+$upload_file3=$uploads_dir.basename($_FILES['img3']['name']);
 
-if(move_uploaded_file($_FILES['img']['tmp_name'],$upload_file)){
+if(move_uploaded_file($_FILES['img']['tmp_name'],$upload_file) && move_uploaded_file($_FILES['img2']['tmp_name'],$upload_file2) && move_uploaded_file($_FILES['img3']['tmp_name'],$upload_file3)){
     echo "<script>alert('데이터 삽입 성공');</script>";}
-else{ echo "<script>alert('데이터 삽입 실패');</script>";}
+else{ echo "<script>alert('데이터 삽입 실패');</script>";} 
 
-$sql = "insert into product(name, pdesc, country, price, brand, img) values('$name', '$pdesc', '$country', '$price', '$brand', '$upload_file')";
-$result = mysqli_query($conn, $sql);
+if ( !mysqli_query ($conn,"insert into product(name, pdesc, country, price, brand, img, img2, img3) values('$name', '$pdesc', '$country', '$price', '$brand', '$upload_file', '$upload_file2', '$upload_file3')") )
 
-if($result === false){
-    echo mysqli_error($conn);
+{
 
- }
+echo("쿼리오류 발생: " . mysqli_error($conn));
+
+} else{
+    echo "잘 들어감1";
+}
 
 
 ?>
